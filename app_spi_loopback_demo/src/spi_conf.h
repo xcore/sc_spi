67102e692f7bab973130edb8fb3457aa682e6cc6
Sam Chesney <samc@whiff.local>
2012-01-20 17:27:58 +0000

    SPI master and slave loopback demo. Replaces separate app_spi_master_demo and app_spi_slave_demo apps, with loopback test that runs on a single XK-1.

---
+++ app_spi_loopback_demo/src/spi_conf.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2011, XMOS Ltd., All rights reserved
+// This software is freely distributable under a derivative of the
+// University of Illinois/NCSA Open Source License posted in
+// LICENSE.txt and at <http://github.xcore.com/>
+
+#define DEFAULT_SPI_CLOCK_DIV 100 //20 //10 //2
+
+#define SPI_MODE 0
+// Ensure that master and slave always operate in the same mode
+#define SPI_MASTER_MODE SPI_MODE
+#define SPI_SLAVE_MODE SPI_MODE
