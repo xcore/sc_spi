348b3602ea5a06e2da717990c7084e15e75510a4
Sam Chesney <samc@whiff.local>
2012-01-20 17:18:24 +0000

    Removed old demo apps

--- app_spi_slave_demo/src/spi_conf.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2011, XMOS Ltd., All rights reserved
-// This software is freely distributable under a derivative of the
-// University of Illinois/NCSA Open Source License posted in
-// LICENSE.txt and at <http://github.xcore.com/>
-
-#define SPI_SLAVE_MODE 3
-
